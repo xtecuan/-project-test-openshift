f96ae85c4efe77671b59ddd3d6284232eb23772a
Tadeo Rivera-Pineda <xtecuan@protonmail.com>
2023-05-18 22:23:59 +0100

    Counter

--- code-with-quarkus/src/main/java/org/acme/GreetingResource.java
+++ code-with-quarkus/src/main/java/org/acme/GreetingResource.java
@@ -4,13 +4,19 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.jboss.logging.Logger;
 
 @Path("/hello")
 public class GreetingResource {
 
+    private static Long counter =0L;
+
+    private static final Logger LOG = Logger.getLogger(GreetingResource.class);
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+        LOG.info("Hello: "+counter++);
         return "Hello RESTEasy Reactive - Openshift";
     }
 }
